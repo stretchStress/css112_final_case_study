57d6d2b0444cb80ebab150d522f5de769c9a65f0
Lawrence Mindanao <mindanaolawrence86@gmail.com>
2025-05-17 22:30:33 +0800

    added model, controller, and modify migration

---
+++ crud_func/database/migrations/2025_05_17_142817_create_movies_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->text('description')->nullable();
+        $table->string('genre');
+        $table->year('release_year');
+        $table->timestamps();
+    });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
